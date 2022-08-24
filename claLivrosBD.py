ccac35923658ff95365cf0da4ef82409b9f48f7f
ManoelSaraiva <manoeljsaraiva@gmail.com>
2022-08-24 00:02:17 -0300

    Criado a classe LivrosBD e as funções de inserir, editar, excluir, listar, buscar e fechar. No arquivo main.py foi criado a tela inicial e a tela de cadastro.

---
+++ claLivrosBD.py
@@ -0,0 +1,51 @@
+"""
+    Classe para conexão ao banco de dados SqlLite3
+"""
+
+import sqlite3 as lite
+
+
+class LivrosBD:
+
+    def __init__(self, arquivo: object) -> object:
+        self.conn = lite.connect(arquivo)
+        self.cursor = self.conn.cursor()
+
+    def inserir(self, titulo: object, autor: object, editora: object, edicao: object, ano: object, local: object, data_compra: object) -> object:
+        consulta = 'INSERT OR IGNORE INTO livros (titulo, autor, editora, edicao, ano, local, data_compra) VALUES (?, ?, ?, ?, ?, ?, ? )'
+        self.cursor.execute(consulta, (titulo, autor, editora, edicao, ano, local, data_compra))
+        self.conn.commit()
+
+    def editar(self, titulo, autor, editora, edicao, ano, local, data_compra, id):
+        consulta = 'UPDATE OR IGNORE livros SET titulo=?, autor=?, editora=?, edicao=?, ano=?, local=?, data_compra=? WHERE id=?'
+        self.cursor.execute(consulta, (titulo, autor, editora, edicao, ano, local, data_compra, id))
+        self.conn.commit()
+
+    def excluir(self, id):
+        consulta = 'DELETE FROM livros WHERE id=?'
+        self.cursor.execute(consulta, (id,))
+        self.conn.commit()
+
+    def listar(self):
+        self.cursor.execute('SELECT * FROM livros')
+
+        for linha in self.cursor.fetchall():
+            print(linha)
+
+    def buscar(self, valor):
+        consulta = 'SELECT * FROM livros WHERE nome LIKE ?'
+        self.cursor.execute(consulta, (f'%{valor}%',))
+
+        for linha in self.cursor.fetchall():
+            print(linha)
+
+    def fechar(self):
+        self.cursor.close()
+        self.conn.close()
+
+
+if __name__ == '__main__':
+    livro = LivrosBD('LivrosBD.db3')
+
+    #livro.inserir('Programação em Python 3', 'Mark Summerfield', 'Alta Books', 'primeira', '2013', 'Casa', '')
+    livro.listar()
---
+++ main.py
@@ -0,0 +1,66 @@
+"""
+    main
+"""
+import os
+import claLivrosBD
+
+livro = claLivrosBD.LivrosBD('LivrosBD.db3')
+
+opcao = 0
+
+
+
+def tela_inicial():
+    print('=========================================================================================================')
+    print('==================================== Cadastro de Livros =================================================')
+    print('=========================================================================================================')
+    print('')
+    print('Digite uma opção:')
+    print('')
+    print('1 - Cadastrar')
+    print('2 - Editar')
+    print('3 - Excluir')
+    print('4 - Listar')
+    print('5 - Buscar')
+    print('6 - Sair')
+    print('')
+    return int(input('Digite sua opção: '))
+
+
+def tela_cadastro():
+    print('=========================================================================================================')
+    print('====================================      Cadastrar     =================================================')
+    print('=========================================================================================================')
+    titulo = input('Digite o titulo do livro: ')
+    print('')
+    autor = input('Digite o autor do livro: ')
+    print('')
+    editora = input('Digite a editora do livro: ')
+    print('')
+    edicao = input('Digite o edicao do livro: ')
+    print('')
+    ano_publ = input('Digite o ano de publicação: ')
+    print('')
+    local = input('Digite o local onde esta guardado: ')
+    print('')
+    data_compra = input('Digite a data de compra: ')
+    livro.inserir(titulo, autor, editora, edicao, ano_publ, local, data_compra)
+
+while opcao != 6:
+    opcao = tela_inicial()
+
+    if opcao == 1:
+        print('1-')
+        os.system('clear') or None
+        tela_cadastro()
+
+    elif opcao == 2:
+        print('2-')
+    elif opcao == 3:
+        print('3')
+    elif opcao == 4:
+        livro.listar()
+    elif opcao == 5:
+        print('5')
+    else:
+        print('Bye')
