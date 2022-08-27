27af4f9c895f8c6eebb7a1b0509bc3efecfdb3e2
ManoelSaraiva <manoeljsaraiva@gmail.com>
2022-08-26 21:28:19 -0300

    Organização do projeto

--- Cadastro/claLivrosBD.py
+++ Cadastro/claLivrosBD.py
@@ -1,24 +1,39 @@
 """
-    Classe para conexão ao banco de dados SqlLite3
+Classe para conexão ao banco de dados SqlLite3
 """
 
 import sqlite3 as lite
 
 
 class LivrosBD:
-
     def __init__(self, arquivo: object) -> object:
         self.conn = lite.connect(arquivo)
         self.cursor = self.conn.cursor()
 
-    def inserir(self, titulo: object, autor: object, editora: object, edicao: object, ano: object, local: object, data_compra: object) -> object:
+    def inserir(
+        self,
+        titulo: object,
+        autor: object,
+        editora: object,
+        edicao: object,
+        ano: object,
+        local: object,
+        data_compra: object,
+    ) -> object:
         consulta = 'INSERT OR IGNORE INTO livros (titulo, autor, editora, edicao, ano, local, data_compra) VALUES (?, ?, ?, ?, ?, ?, ? )'
-        self.cursor.execute(consulta, (titulo, autor, editora, edicao, ano, local, data_compra))
+        self.cursor.execute(
+            consulta, (titulo, autor, editora, edicao, ano, local, data_compra)
+        )
         self.conn.commit()
 
-    def editar(self, titulo, autor, editora, edicao, ano, local, data_compra, id):
+    def editar(
+        self, titulo, autor, editora, edicao, ano, local, data_compra, id
+    ):
         consulta = 'UPDATE OR IGNORE livros SET titulo=?, autor=?, editora=?, edicao=?, ano=?, local=?, data_compra=? WHERE id=?'
-        self.cursor.execute(consulta, (titulo, autor, editora, edicao, ano, local, data_compra, id))
+        self.cursor.execute(
+            consulta,
+            (titulo, autor, editora, edicao, ano, local, data_compra, id),
+        )
         self.conn.commit()
 
     def excluir(self, id):
@@ -47,5 +62,5 @@ class LivrosBD:
 if __name__ == '__main__':
     livro = LivrosBD('LivrosBD.db3')
 
-    #livro.inserir('Programação em Python 3', 'Mark Summerfield', 'Alta Books', 'primeira', '2013', 'Casa', '')
+    # livro.inserir('Programação em Python 3', 'Mark Summerfield', 'Alta Books', 'primeira', '2013', 'Casa', '')
     livro.listar()
